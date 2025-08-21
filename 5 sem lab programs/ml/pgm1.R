6f7b301a65be37df16463c10a3ec7c3f54b7a367
Maruthi-byte <207844235+Maruthi-byte@users.noreply.github.com>
2025-08-21 19:03:49 +0530

    Add files via upload

---
+++ 5 sem lab programs/ml/pgm1.R
@@ -0,0 +1,12 @@
+name <- readline(prompt = "Enter your name: ") 
+print(paste("Hello,", name)) 
+x <- 5 
+y <- 3.14 
+text <- "Hello" 
+my_list <- list(1, 2, 3) 
+my_df <- data.frame(a = 1:3, b = 4:6) 
+print(x) 
+print(y) 
+print(text) 
+print(my_list) 
+print(my_df)
